9581a5901b72e459ed39193ec5dc4eec9766297c
NataliaAndenko <120939798+NataliaAndenko@users.noreply.github.com>
2023-02-12 18:21:33 +0300

    Add files via upload

---
+++ Program02.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int M = InputNumbers("Введите число M (первое): ");
+int N = InputNumbers("Введите число N (второе): ");
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+void Sum(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    Sum(M, N, sum);
+}
+Sum(M, N, 0);
